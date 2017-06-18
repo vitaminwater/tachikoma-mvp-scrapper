eb8cfb89de47598a5fa08259da2f3bfaa124605c
vitaminwater <stantin04@gmail.com>
2017-06-18 22:11:01 +0200

    fix log

--- index.js
+++ index.js
@@ -2,17 +2,16 @@ const cheerio = require('cheerio');
 const rp = require('request-promise');
 
 [...Array(105).keys()].reduce((promise, page) => {
-  const loader = (arr) => {
-    console.log(`loading page ${page}`);
-    return rp({
-      uri: `https://apps.shopify.com/oberlo?page=${page}`,
-      transform: function (body) {
-        return cheerio.load(body);
-      }
-    }).then(($) => arr.concat($('[itemprop=author]').map((i, author) => {
-      return `${$(author).attr('href')}/collections/all?sort_by=best-selling`;
-    }).get()))
-  };
+  const loader = (arr) => rp({
+    uri: `https://apps.shopify.com/oberlo?page=${page}`,
+    transform: function (body) {
+      return cheerio.load(body);
+    }
+  })
+  .then(($) => arr.concat($('[itemprop=author]').map((i, author) => {
+    return `${$(author).attr('href')}/collections/all?sort_by=best-selling`;
+  }).get()))
+  .then((arr) => { console.log(`loaded page ${page}`); return arr})
   return promise.then(loader);
 }, Promise.resolve([]))
 .then((pages) => {
